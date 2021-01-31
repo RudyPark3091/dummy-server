aca3ed649b8be84b66e69751062f9f3c5219f3ad
RudyPark3091 <grayblack313@gmail.com>
2021-01-31 22:52:41 +0900

    Update todos.controller.js
    added a few functions for todos api

--- src/routes/api/todos/index.js
+++ src/routes/api/todos/index.js
@@ -4,7 +4,19 @@ import controller from "./todos.controller";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("this will return todo items");
+	const data = controller.findAll();
+	res.send(data);
 });
+
+router.get("/:id", (req, res) => {
+	const data = controller.findById(req.params.id);
+	res.send(data);
+})
+
+router.post("/", (req, res) => {
+	const body = req.body;
+	controller.create(body);
+	res.send(body);
+})
 
 export default router;
--- src/routes/api/todos/todos.controller.js
+++ src/routes/api/todos/todos.controller.js
@@ -1,1 +1,35 @@
-import express from "express";
+const db = [
+  {
+    id: 0,
+    content: "lorem ipsum",
+    due: "2021-02-01",
+  },
+  {
+    id: 1,
+    content: "dolor sit",
+    due: "2021-02-01",
+  }
+];
+
+const controller = {
+  findAll: _ => {
+    return db;
+  },
+  findById: id => {
+    for (const item of db) {
+      if (item.id === +id) return item;
+    }
+  },
+  create: item => {
+    if (item.id)
+      throw new Error("you cannot specify id");
+    if (!item.content)
+      throw new Error("there must be a content");
+    if (!item.due)
+      throw new Error("there must be a due date");
+    item.id = db.length;
+    db.push(item);
+  },
+}
+
+export default controller;
