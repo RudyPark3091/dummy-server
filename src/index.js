d39f976353b2589538033b86a3420937a64e36ba
rudy3091 <grayblack313@gmail.com>
2021-05-16 00:50:15 +0900

    Disable cors for all origins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import apiRouter from "./routes/api/";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
