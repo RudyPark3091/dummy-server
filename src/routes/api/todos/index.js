3fb5398f623b61fbb74345f0ff9e12f0400621d5
rudy3091 <grayblack313@gmail.com>
2021-08-24 12:47:26 +0900

    Update todos/index.js, todos.controller.js
    replace one tab character into two whitespaces

--- src/routes/api/todos/index.js
+++ src/routes/api/todos/index.js
@@ -4,37 +4,42 @@ import controller from "./todos.controller";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	const data = controller.findAll();
-	res.status(200).send(data);
+  const data = controller.findAll();
+  res.status(200).send(data);
 });
 
 router.get("/:id", (req, res) => {
-	const data = controller.findById(req.params.id);
-	if (!data)
-		res.status(404).send({ message: `item with ${req.params.id} not found` });
-	else
-		res.status(200).send(data);
+  const data = controller.findById(req.params.id);
+  if (!data)
+    res
+      .status(404)
+      .send({ message: `item with ${req.params.id} not found` });
+  else res.status(200).send(data);
 });
 
 router.post("/", (req, res) => {
-	const body = req.body;
-	const created = controller.create(body);
-	res.status(201).json({ id: created.id, message: "created" });
+  const body = req.body;
+  const created = controller.create(body);
+  res.status(201).json({ id: created.id, message: "created" });
 });
 
 router.put("/:id", (req, res) => {
-	const body = req.body;
-	if (controller.update(+(req.params.id), body))
-		res.status(200).json({ id: req.params.id, message: "updated" });
-	else
-		res.status(404).json({ message: `item with ${req.params.id} not found` });
-})
+  const body = req.body;
+  if (controller.update(+req.params.id, body))
+    res.status(200).json({ id: req.params.id, message: "updated" });
+  else
+    res
+      .status(404)
+      .json({ message: `item with ${req.params.id} not found` });
+});
 
 router.delete("/:id", (req, res) => {
-	if (controller.delete(+(req.params.id)))
-		res.status(200).json({ id: req.params.id, message: "deleted" });
-	else
-		res.status(404).json({ message: `item with ${req.params.id} not found`});
-})
+  if (controller.delete(+req.params.id))
+    res.status(200).json({ id: req.params.id, message: "deleted" });
+  else
+    res
+      .status(404)
+      .json({ message: `item with ${req.params.id} not found` });
+});
 
 export default router;
--- src/routes/api/todos/todos.controller.js
+++ src/routes/api/todos/todos.controller.js
@@ -10,54 +10,53 @@ let db = [
     content: "dolor sit",
     due: "2021-02-01",
     done: true,
-  }
+  },
 ];
 
 const controller = {
-  findAll: _ => {
+  findAll: (_) => {
     return db;
   },
-  findById: id => {
-    const filtered = db.filter(item => item.id === +id);
+  findById: (id) => {
+    const filtered = db.filter((item) => item.id === +id);
     if (filtered.length !== 1) return false;
     else return filtered[0];
   },
-  create: body => {
-    if (body.id)
-      throw new Error("you cannot specify id");
-    if (!body.content)
-      throw new Error("there must be a content");
-    if (!body.due)
-      throw new Error("there must be a due date");
-    if (!body.hasOwnProperty("done") || typeof body.done !== "boolean")
+  create: (body) => {
+    if (body.id) throw new Error("you cannot specify id");
+    if (!body.content) throw new Error("there must be a content");
+    if (!body.due) throw new Error("there must be a due date");
+    if (
+      !body.hasOwnProperty("done") ||
+      typeof body.done !== "boolean"
+    )
       throw new Error("boolean field done cannot be a null");
     const item = {
       id: db.length,
-      ...body
+      ...body,
     };
     db.push(item);
-		return item;
+    return item;
   },
   update: (id, body) => {
-    if (body.id)
-      throw new Error("you cannot specify id");
-    const changed = db.filter(item => {
+    if (body.id) throw new Error("you cannot specify id");
+    const changed = db.filter((item) => {
       if (item.id === id) {
-				if (body.content) item.content = body.content;
-				if (body.due) item.due = body.due;
-				if (body.hasOwnProperty("done")) item.done = body.done;
+        if (body.content) item.content = body.content;
+        if (body.due) item.due = body.due;
+        if (body.hasOwnProperty("done")) item.done = body.done;
         return true;
       }
     });
-		if (changed.length > 0) return true;
+    if (changed.length > 0) return true;
     else return false;
   },
-  delete: id => {
+  delete: (id) => {
     const l = db.length;
-    db = db.filter(item => item.id !== id);
+    db = db.filter((item) => item.id !== id);
     if (l > db.length) return true;
     else return false;
-  }
-}
+  },
+};
 
 export default controller;
